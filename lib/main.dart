1a6994499239f72df079892ce06d398c5bac93e2
Kishor Kc <kishorkc8007@gmail.com>
2021-05-31 23:18:27 +0545

    starter riverpod day-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
-import 'package:exploring_riverpod/page/basicPage.dart';
+import 'package:exploring_riverpod/page/switchPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: Day3()));
 }
 
-class MyApp extends StatelessWidget {
+class Day3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Day 2",
+      title: "Day 3",
       theme: ThemeData(
-        canvasColor: Colors.purpleAccent.withOpacity(0.3),
+        canvasColor: Colors.teal.withOpacity(0.3),
       ),
       debugShowCheckedModeBanner: false,
-      home: DataTypePage(),
+      home: SwitchHomePage(),
     );
   }
 }
